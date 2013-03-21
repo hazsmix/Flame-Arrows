d812c80c9c32498a9eca5379399bf632080b9b73
Harley Smithers <c3145161@uon.edu.au>
2013-03-21 22:41:26 +1100

    Added comments and fixed imports
    Signed-off-by: Harley Smithers <c3145161@uon.edu.au>

--- src/me/hsmithers/FlameArrows.java
+++ src/me/hsmithers/FlameArrows.java
@@ -6,9 +6,10 @@ import org.bukkit.entity.Arrow;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Projectile;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class FlameArrows extends JavaPlugin {
+public class FlameArrows extends JavaPlugin { // the main (and only) class
 
 	static String PLUGIN_PREFIX = "Flame Arrows";
 	static String VERSION = "v0.1";
@@ -23,16 +24,39 @@ public class FlameArrows extends JavaPlugin {
 				PLUGIN_PREFIX + " " + VERSION + "has been disabled");
 	}
 
-	@EventHandler
-	public void projectileHitEvent(Projectile projectile) {
-		if (projectile.getShooter() instanceof Player) {
-			Player player = (Player) projectile.getShooter();
-			if (player.hasPermission("flamearrow.ignite")) {
-				if (projectile instanceof Arrow) {
-					if (projectile.getFireTicks() > 0) {
-						projectile.getLocation().getBlock()
-								.getRelative(BlockFace.UP)
-								.setType(Material.FIRE);
+	@EventHandler(priority = EventPriority.LOW)
+	// lets other plugins manage this event as well
+	public void projectileHitEvent(Projectile projectile) { // receives all
+															// events when an
+															// arrow hits
+															// "something" - a
+															// block, player, or
+															// entity
+		if (projectile.getShooter() instanceof Player) { // don't want to have
+															// skeletons with
+															// flaming arrows...
+															// or do we?
+			Player player = (Player) projectile.getShooter(); // stores the
+																// shooter, and
+																// casts to
+																// player
+			if (player.hasPermission("flamearrow.ignite")) { // checks
+																// permissions
+				if (projectile instanceof Arrow) { // checks the projectile is
+													// actually an arrow (should
+													// this be first to reduce
+													// unnecessary firing of the
+													// event?)
+					if (projectile.getFireTicks() > 0) { // if the arrow is
+															// already on fire
+															// (ie shot through
+															// a flame)
+						projectile.getLocation().getBlock() // gets the "block"
+															// the arrow is
+															// "mostly" in
+								.getRelative(BlockFace.UP) // gets the top of
+															// this block
+								.setType(Material.FIRE); // and sets it on fire
 					}
 				}
 			}
